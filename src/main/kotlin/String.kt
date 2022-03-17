d720601d03c01c3d5ea7dd531a576033244f3731
nursafika <nursafika054@gmail.com>
2022-03-17 11:12:37 +0700

    Contoh kode program mengakses elemen String

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val myName: String = "Nursafika"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+
+    val s = "abc" + 1
+    println(s + "def")
+}
